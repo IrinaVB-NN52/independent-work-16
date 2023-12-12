c171fb2c9773c7a7ec28aeccc8be41e6e42bd637
Ирина Баранова <irina.baranova14@outlook.com>
2023-12-12 23:19:04 +0300

    Добавьте файлы проекта.

---
+++ Задача 16.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Задача_16._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Product.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            Product maxProduct = products[0];
+            foreach (Product e in products)
+
+            {
+                if(e.Sum > maxProduct.Sum )
+                {
+                    maxProduct = e;
+                }
+            }
+            Console.WriteLine($"{maxProduct.Code } {maxProduct.Name } {maxProduct.Sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 16/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+namespace Задача_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] products = new  Product[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Введите код товара");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите наименование товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара");
+                int sum = Convert.ToInt32(Console.ReadLine());
+
+                products[i] = new Product() { Code = code, Name = name, Sum =sum };
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                    Encoder=JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                    WriteIndented = true
+            };
+             string jsonString = JsonSerializer.Serialize(products, options);
+
+            using (StreamWriter sw = new StreamWriter ("../../../Product.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
